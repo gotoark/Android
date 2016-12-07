f9d6f034ed22ab73e3d1023f3f63c4e14af2bb1b
Rajesh Kumar Arumugam <gotoark@gmail.com>
2016-12-07 23:34:53 +0530

    AdMobBasic Added

---
+++ AdMobBasic/java/com/android/arkthepro/admobbasic/MainActivity.java
@@ -0,0 +1,22 @@
+package com.android.arkthepro.admobbasic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
+        //This is Google Test Ads ID: ca-app-pub-3940256099942544~3347511713" Replace with Your app ad unit ID Here
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
